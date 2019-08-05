e86ef5863ca8cb958e2879517c422bfb3f8da4cb
Peter-John MEYER <pemeyer@c2r2s1.wethinkcode.co.za>
2019-08-05 14:59:07 +0200

    new shit

--- src/main/java/com/pemeyer/swingy/App.java
+++ src/main/java/com/pemeyer/swingy/App.java
@@ -22,11 +22,12 @@ public class App
 {
     JFrame window;
     Container con;
-    JPanel titleNamePanel, createButtonPanel;
+    JPanel titleNamePanel, createButtonPanel, mainTextPanel, choiceButtonPanel;
     JLabel titleNameLabel;
+    JTextArea mainTextArea;
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 28);
-    JButton createButton;
+    JButton createButton, createHero, chooseSavedHero;
 
     TitleScreenHandler tsHandler = new TitleScreenHandler();
 
@@ -45,7 +46,6 @@ public class App
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
-        window.setVisible(true);
         con = window.getContentPane();
 
         titleNamePanel = new JPanel();
@@ -61,9 +61,9 @@ public class App
 
         createButton = new JButton("CREATE GAME");
         createButton.setBackground(Color.black);
-        createButton.setForeground(Color.white);
+        createButton.setForeground(Color.white );
         createButton.setFont(normalFont);
-        //createButton.addActionListener(tsHandler);
+        createButton.addActionListener(tsHandler);
         createButton.setFocusPainted(false);
 
         titleNamePanel.add(titleNameLabel);
@@ -72,12 +72,49 @@ public class App
         con.add(titleNamePanel);
         con.add(createButtonPanel);
 
+        window.setVisible(true);
     }
     public void createCreateScreen() {
         titleNamePanel.setVisible(false);
         createButtonPanel.setVisible(false);
-    }
 
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
+        mainTextPanel.setBackground(Color.black);
+        
+        mainTextArea = new JTextArea("CREATE YOUR HERO HERE:");
+        mainTextArea.setBounds(100, 100, 600, 250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
+
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(250, 350, 300, 150);
+        choiceButtonPanel.setBackground(Color.black);
+        choiceButtonPanel.setLayout(new GridLayout(2,1));
+        
+        createHero = new JButton("CREATE NEW HERO");
+        createHero.setBackground(Color.black);
+        createHero.setForeground(Color.white);
+        createHero.setFont(normalFont);
+        createHero.setFocusPainted(false);
+
+        chooseSavedHero = new JButton("CHOOSE SAVED HERO");
+        chooseSavedHero.setBackground(Color.black);
+        chooseSavedHero.setForeground(Color.white);
+        chooseSavedHero.setFont(normalFont);
+        chooseSavedHero.setFocusPainted(false);
+
+        choiceButtonPanel.add(createHero);
+        choiceButtonPanel.add(chooseSavedHero);
+
+        con.add(mainTextPanel);
+        con.add(choiceButtonPanel);
+
+    }
+    
     public class TitleScreenHandler implements ActionListener {
         public void actionPerformed(ActionEvent event){
             createCreateScreen();
